9aaf74c4abc23af9db8a15416fb669aa64e56061
mrtan <freemrtan@gmail.com>
2020-01-03 17:08:50 +0800

    add row && column

---
+++ lib/layout_basic/row_colum.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RowAndColum extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Row && Colum"),
+      ),
+      body: Row(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
